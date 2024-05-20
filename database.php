ae77c98f58246db9cb70f62346d527a7196bdfa9
Taona Munikwa <69734394+taonaben@users.noreply.github.com>
2024-05-20 22:36:47 +0200

    second commit

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new SQLite3('Tayamika.db');
-
-$result = $db->query('SELECT * FROM your_table');
-while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-    echo $row['column_name'] . "<br>";
-}
-?>;
-/home/yochyy/Desktop/2.2 Project/database.php
