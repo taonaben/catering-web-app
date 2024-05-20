0cdcd4c495e7a96ead32f4431597ba1f23413428
Taona Munikwa <69734394+taonaben@users.noreply.github.com>
2024-05-20 14:59:07 +0200

    initial commit

---
+++ headchef_dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Headchef Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <a href="#" class="logo"><i class="fas fa-utensils"></i>Tayamika Catering Head Chef Dashboard</a>
+    </header>
+    <section class="dashboard">
+        <h1>Headchef Dashboard</h1>
+        <p>This is an empty dashboard for the Headchef.</p>
+    </section>
+</body>
+</html>
---
+++ manager_dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manager Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <a href="#" class="logo"><i class="fas fa-utensils"></i>Tayamika Catering Manager Dashboard</a>
+    </header>
+    <section class="dashboard">
+        <h1>Manager Dashboard</h1>
+        <p>This is an empty dashboard for the Manager.</p>
+    </section>
+</body>
+</html>
